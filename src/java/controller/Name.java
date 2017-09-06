e1081c569fddbbf0a99acc7c7093df2e7deb9378
Viktor <vjh2@hi.is>
2017-09-06 17:04:25 +0000

    Skjolun

---
+++ src/java/controller/Name.java
@@ -0,0 +1,18 @@
+package controller;
+
+/**
+ * Getters og setters fyrir nafn notanda
+ * @author Viktor Jón Helgason vjh2@hi.is
+ */
+public class Name {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/java/service/HelloService.java
@@ -0,0 +1,12 @@
+package service;
+
+/**
+ * Service sem segir Halló Nafn við notanda
+ * @author Viktor Jón Helgason vjh2@hi.is
+ */
+public class HelloService {
+
+    public String sayHello(String name) {
+        return "Halló " + name + "!";
+    }
+}
